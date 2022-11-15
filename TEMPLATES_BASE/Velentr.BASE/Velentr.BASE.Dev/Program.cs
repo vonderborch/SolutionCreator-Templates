229b1f14c06d45d78cc107b74e64e37705650ede
Christian Webber <ricky.webber@hotmail.com>
2022-11-15 18:58:07 -0500

    Changes and new template updates

---
+++ TEMPLATES_BASE/Velentr.BASE/Velentr.BASE.Dev/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Velentr.BASE.dev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // insert test code here
+        }
+    }
+}
