0abed72ddb04e06c456bf42e12a9c0fae9c28170
Christian Webber <ricky.webber@hotmail.com>
2022-11-16 17:54:49 -0500

    template changes

---
+++ TEMPLATES_BASE/Velentr.DUAL_SUPPORT_WITH_GENERIC/test/manual/Velentr.DevEnv/TestCode.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Velentr.DevEnv
+{
+    public class TestCode
+    {
+        public GraphicsDevice GraphicsDevice;
+        public GraphicsDeviceManager Graphics;
+        public SpriteBatch SpriteBatch;
+
+        public string LIBRARY = "Velentr.DUAL_SUPPORT_WITH_GENERIC";
+
+        public TestCode(GraphicsDeviceManager graphics)
+        {
+            Graphics = graphics;
+
+            // Class initialization code goes here
+        }
+
+        public void Initialize()
+        {
+            // Initialization code goes here
+        }
+        public void LoadContent()
+        {
+            // LoadContent code goes here
+        }
+        public void Update(GameTime gameTime)
+        {
+            // Update code goes here
+        }
+        public void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // Draw code goes here
+        }
+    }
+}
